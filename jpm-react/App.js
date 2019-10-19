7e8f597e76c1e2ee8ee098c019874056501433c7
Vicky Yu <vickyyu@usc.edu>
2019-10-19 07:16:34 -0700

    twilio api

--- jpm-react/App.js
+++ jpm-react/App.js
@@ -6,9 +6,32 @@ import { createAppContainer } from 'react-navigation';
 
 import { createStackNavigator } from 'react-navigation-stack';
 
- 
+import logo from './inroads_transparent.png';
+
+import SMSForm from './SMSForm';
 
 class HomeScreen extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      greeting: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+   handleChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
+      .then(response => response.json())
+      .then(state => this.setState(state));
+  }
 
   render() {
 
@@ -39,6 +62,14 @@ class HomeScreen extends React.Component {
           onPress={() => this.props.navigation.navigate('Alumni')}
 
         />
+
+        <div className="App">
+        <header className="App-header">
+          <img src={logo} />
+
+          <SMSForm />
+        </header>
+      </div>
 
       </View>
 
