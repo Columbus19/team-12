5d79344d1764bf7584be60eb3c59461e4197e6f7
Danny Lee <47467806+datwony@users.noreply.github.com>
2019-10-19 00:32:56 -0400

    updated app.js with routing

--- jpm-react/App.js
+++ jpm-react/App.js
@@ -1,18 +1,221 @@
 import React from 'react';
-import { StyleSheet, Text, View , Button} from 'react-native';
-import HomeScreen from './pages/home_screen.js'
 
-export default function App() {
-  return (
-    <HomeScreen/>
-  );
+import { View, Text, Button } from 'react-native';
+
+import { createAppContainer } from 'react-navigation';
+
+import { createStackNavigator } from 'react-navigation-stack';
+
+ 
+
+class HomeScreen extends React.Component {
+
+  render() {
+
+    return (
+
+      <View style={{ flex: 3, alignItems: 'center', justifyContent: 'center' }}>
+
+        <Button
+
+          title="Student"
+
+          onPress={() => this.props.navigation.navigate('Student')}
+
+        />
+
+        <Button
+
+          title="Employer"
+
+          onPress={() => this.props.navigation.navigate('Employer')}
+
+        />
+
+        <Button
+
+          title="Alumni"
+
+          onPress={() => this.props.navigation.navigate('Alumni')}
+
+        />
+
+      </View>
+
+    );
+
+  }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+class StudentScreen extends React.Component {
+
+  render() {
+
+    return (
+
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+        <Text>Student Screen</Text>
+
+        <Button
+
+          title="Log In"
+
+          onPress={() => this.props.navigation.navigate('LogIn')}
+
+        />
+
+        <Button
+
+          title="Register"
+
+          onPress={() => this.props.navigation.navigate('Register')}
+
+        />
+
+      </View>
+
+    );
+
+  }
+
+}
+
+class AlumniScreen extends React.Component {
+
+  render() {
+
+    return (
+
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+        <Text>Alumni Screen</Text>
+
+        <Button
+
+          title="Log In"
+
+          onPress={() => this.props.navigation.navigate('LogIn')}
+
+        />
+
+        <Button
+
+          title="Register"
+
+          onPress={() => this.props.navigation.navigate('Register')}
+
+        />
+
+      </View>
+
+    );
+
+  }
+
+}
+
+class EmployerScreen extends React.Component {
+
+  render() {
+
+    return (
+
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+        <Text>Employer Screen</Text>
+
+        <Button
+
+          title="Log In"
+
+          onPress={() => this.props.navigation.navigate('LogIn')}
+
+        />
+
+        <Button
+
+          title="Register"
+
+          onPress={() => this.props.navigation.navigate('Register')}
+
+        />
+
+      </View>
+
+    );
+
+  }
+
+}
+
+class LogInScreen extends React.Component {
+
+  render() {
+
+    return (
+
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+        <Text>LogIn Screen</Text>
+
+       
+
+      </View>
+
+    );
+
+  }
+
+}
+
+class RegisterScreen extends React.Component {
+
+  render() {
+
+    return (
+
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+        <Text>Register Screen</Text>
+
+       
+
+      </View>
+
+    );
+
+  }
+
+}
+
+const AppNavigator = createStackNavigator(
+
+  {
+
+    Home: HomeScreen,
+
+    Student: StudentScreen,
+
+    Employer: EmployerScreen,
+
+    Alumni: AlumniScreen,
+
+    LogIn: LogInScreen,
+
+    Register: RegisterScreen
+
   },
-});
+
+  {
+
+    initialRouteName: 'Home',
+
+  }
+
+);
+
+ 
+
+export default createAppContainer(AppNavigator);
